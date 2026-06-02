ad2e83917ec640ef2ca249c43651a43d0d421e70
Wagner Loch <wagner_loch@hotmail.com>
2026-06-02 20:38:06 -0300

    Atualização de mensagem

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World!');
+    res.send('Olá mundo! Deploy automático com CI/CD');
 });
 
 app.listen(port, () => {
